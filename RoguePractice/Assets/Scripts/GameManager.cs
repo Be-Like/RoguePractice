a1fa42a203c4e2155af7dba4e1d9e03d875bd05c
Jacob Simpson <Jacob.Simpson@ssi-schaefer.us>
2019-05-10 16:30:20 -0400

    Added User and Enemy Functionality

--- RoguePractice/Assets/Scripts/GameManager.cs
+++ RoguePractice/Assets/Scripts/GameManager.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    public float turnDelay = .1f;
     public static GameManager instance = null;
-    private BoardManager boardScript;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
     void Awake()
     {
@@ -21,19 +28,54 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
-    
-        boardScript = GetComponent<BoardManager>();
 
+        // Initializers
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 
     private void Update()
     {
+        if (playersTurn || enemiesMoving)
+            return;
 
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
